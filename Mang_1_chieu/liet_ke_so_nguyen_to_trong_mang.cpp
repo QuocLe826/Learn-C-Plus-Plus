334444c41984f20778d60286c5494f439ed3e063
Lê Văn Quốc <57262051+QuocLe826@users.noreply.github.com>
2026-02-13 14:00:04 +0700

    Create liet_ke_so_nguyen_to_trong_mang.cpp

---
+++ Mang_1_chieu/liet_ke_so_nguyen_to_trong_mang.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int isPrime(int num) {
+    if(num < 2) {
+        return 0;
+    }
+    for(int i = 2; i <= sqrt(num); i++) {
+        if(num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+void input(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << "Nhap phan tu thu " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+}
+
+void output(int arr[], int n) {
+    cout << "Cac so nguyen to trong mang la: ";
+    for(int i = 0; i < n; i++) {
+        if(isPrime(arr[i])) {
+            cout << arr[i] << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+
+    int quantity;
+    cout << "Nhap so luong phan tu: ";
+    cin >> quantity;
+    int arr[quantity];
+    input(arr, quantity);
+    output(arr, quantity);
+
+    system("pause");
+    return 0;
+}
